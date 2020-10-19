8cc0909fdd165cf24f331fee06f6c613306014a8
ZabdielReyes <70989008+ZabdielReyes@users.noreply.github.com>
2020-10-19 14:41:30 -0500

    Operador actualizado

--- computadora.h
+++ computadora.h
@@ -1,6 +1,7 @@
 #ifndef COMPUTADORA_H_INCLUDED
 #define COMPUTADORA_H_INCLUDED
 #include<iostream>
+#include <iomanip>
 using namespace std;
 
 class Computadora
@@ -23,6 +24,42 @@ public:
 	float getGb();
 	void setRam(int v);
 	int getRam();
+	
+	friend ostream& operator<<(ostream &out, const Computadora &p)
+	{
+		out << left ;
+		out << setw(12) << p.so ;
+		out << setw(12) << p.tipo ;
+		out << setw(12) << p.gb;
+		out << setw(10) << p.ram;
+		out <<endl;
+		
+		return out;
+	}
+	
+	friend istream& operator >>(istream &in, Computadora &p)
+	{
+		/*
+		string temp;
+		float gb;
+		int ram;
+		*/
+		
+		cout <<"SO: ";
+		getline(cin, p.so);
+		
+		cout <<"Marca: ";
+		getline(cin, p.tipo);
+		
+		cout <<"Almacenamiento: ";
+		cin>> p.gb;
+		
+		cout <<"Ram: ";
+		cin>> p.ram;
+		
+		return in;
+	}
+	
 };
 
 #endif
