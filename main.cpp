fb135691224b47aa2685c74f59b928b8e844750c
ZabdielReyes <70989008+ZabdielReyes@users.noreply.github.com>
2020-10-19 22:41:13 -0500

    Respaldar() añadido

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ using namespace std;
 
 int main(int argc, char** argv) 
 {
+	//Laboratorio l;
+	//	l.recuperar();
+	//	l.mostrar();
+
 	system("cls");
 	
 	Computadora c01;
@@ -32,24 +36,37 @@ int main(int argc, char** argv)
 	c03.setGb(481.2);
 	c03.setRam(12);
 	
-	Computadora c04;
+	/*Computadora c04;
 	c04.setSo("WIndows");
 	c04.setTipo("Asus");
 	c04.setGb(2060.80);
 	c04.setRam(32);
+	*/
 	
 	Computadora c05;
 	c05.setSo("Windows");
 	c05.setTipo("Dell");
 	c05.setGb(60.2);
 	c05.setRam(4);
-	
+
 	Laboratorio lab;
 	lab.agregarComputadora(c01);
 	lab.agregarComputadora(c02);
 	lab.agregarComputadora(c03);
-	lab.agregarComputadora(c04);
+	//lab.agregarComputadora(c04);
 	lab.agregarComputadora(c05);
-	lab.mostrar();
 	
+	
+	//cout << c01;
+	Computadora c04;
+	cin >> c04;
+	//----Forma 1------
+	//lab.agregarComputadora(c04);
+	// ---------Forma 2------ (Agregando funcion Friend a laboratorio.h)
+	lab << c04;
+	//MOSTRAR
+	lab.mostrar();
+	lab.respaldar_tabla();
+	lab.respaldar(); //linea por linea
+
 }
